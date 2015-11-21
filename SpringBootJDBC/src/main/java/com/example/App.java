3e2f03292cc6dd623e407c9b410aca3ffc759cb4
Taiki <tyoshikawa1106@gmail.com>
2015-11-21 17:24:37 +0900

    commit

--- SpringBootJDBC/src/main/java/com/example/App.java
+++ SpringBootJDBC/src/main/java/com/example/App.java
@@ -1,33 +1,27 @@
 package com.example;
 
 import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class App implements CommandLineRunner {
     @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
+    CustomerRepository customerRepository;
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-        SqlParameterSource param = new MapSqlParameterSource()
-                .addValue("id", 1);
-        Customer result = jdbcTemplate.queryForObject(sql, param,
-                (rs, rowNum) -> new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name")) // (1)
-        );
-
-        System.out.println("result = " + result);
+        // データ追加
+        Customer created = customerRepository.save(new Customer(null, "Sugio", "Deki"));
+        System.out.println(created + " is created!");
+        // データ表示
+        customerRepository.findAll()
+                .forEach(System.out::println);
     }
 
     public static void main(String[] args) {
--- SpringBootJDBC/src/main/resources/schema.sql
+++ SpringBootJDBC/src/main/resources/schema.sql
@@ -1,1 +1,1 @@
-CREATE table customers(id int primary key auto_increment, first_name varchar(30), last_name varchar(30));
+CREATE table IF NOT EXISTS customers(id int primary key auto_increment, first_name varchar(30), last_name varchar(30));
