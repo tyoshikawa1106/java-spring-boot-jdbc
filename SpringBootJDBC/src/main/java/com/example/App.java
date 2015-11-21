1a87d92589c9505b92be6ba6e9a520f0dfbc14a6
Taiki <tyoshikawa1106@gmail.com>
2015-11-21 13:46:41 +0900

    commit

--- SpringBootJDBC/src/main/java/com/example/App.java
+++ SpringBootJDBC/src/main/java/com/example/App.java
@@ -1,23 +1,34 @@
 package com.example;
 
+import com.example.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 @EnableAutoConfiguration
 public class App implements CommandLineRunner {
     @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate; // (1)
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... strings) throws Exception {
-        String sql = "SELECT 1"; // (2)
-        SqlParameterSource param = new MapSqlParameterSource(); // (3)
-        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class); // (4)
+        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("id", 1);
+        Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+            @Override
+            public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+                return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+            }
+        });
 
         System.out.println("result = " + result);
     }
